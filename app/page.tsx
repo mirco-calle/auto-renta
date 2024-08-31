235785df379f5e4d511e055cf73d55d208802b19
mirco <mirkex.guitar@gmail.com>
2024-08-31 11:19:50 -0400

    primer commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p>mirco</p>
+      <Button>This is a button </Button>
+    </div>
+  );
+}
